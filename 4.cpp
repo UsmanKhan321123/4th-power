d0f34c554c4fbb620f92640916444ee803db8fed
UsmanKhan321123 <uk732161@gmail.com>
2024-04-17 23:55:45 +0500

    4th power using function

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int func(){
+	int num;
+	cout<<"plz enter a number";
+	cin>>num;
+	cout<<"The number is : "<<num<<endl;
+	cout<<"The 4th power of this number is : "<<num*num*num*num<<endl;
+}
+
+int main(){
+	func();
+};
